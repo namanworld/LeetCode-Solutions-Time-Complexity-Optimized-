d374b2533e2f224e4bec772b9635ea55c3510030
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-04-29 23:41:46 +0530

    Update 1002. Find Common Characters.cpp

--- 1002. Find Common Characters.cpp
+++ 1002. Find Common Characters.cpp
@@ -31,3 +31,31 @@ public:
         
     }
 };
+
+
+
+
+O(N) approach
+
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        int sum = 0;
+        for(int i=0 ; i<words.size() ; i++){
+            string s = words[i];
+            string x = chars;
+            int j;
+            bool found = true;
+            for(j=0 ; j<s.length() ; j++){
+                int a = x.find(s[j]);
+                if(a!=-1) x[a] = '-';
+                else {
+                    found = false;
+                    break;
+                }
+            }
+            if(j==s.length() && found) sum+=s.length();
+        }
+        return sum;
+    }
+};
