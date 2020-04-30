91261050dcf4c4888743a32c3f85960e31c1afac
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-01 03:47:09 +0530

    Better solution

--- 124. Binary Tree Maximum Path Sum.cpp
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    #include <bits/stdc++.h>
-    
-    int helper(TreeNode* root, int &ans){
-        if(!root) return 0;
-        
-        int leftAns = helper(root->left, ans);
-        int rightAns = helper(root->right, ans);
-        
-        int maxSum = max(max(leftAns, rightAns)+root->val, root->val);
-        
-        int maxTop = max(maxSum, leftAns + rightAns + root->val);
-        ans = max(ans, maxTop);
-        return maxSum;
-        
+    int maxPathSum(TreeNode* root) 
+    {
+        int max = -999999;        
+        partialMaxPathSum(root, max);
+        return max;
     }
     
-    int maxPathSum(TreeNode* root) {
-
-        int ans = INT_MIN;
-        helper(root, ans);
-        return ans;
+    int  partialMaxPathSum(TreeNode* root, int& max) 
+    {
+        if(root == nullptr)
+        {
+            return 0;    
+        }
+      
+        int left = partialMaxPathSum(root->left, max);
+        int right = partialMaxPathSum(root->right, max);        
+        
+        max = std::max(left+right+root->val, max);
+        int ret = root->val + std::max(left, right);
+        return ret > 0 ? ret : 0;        
     }
+    
 };
