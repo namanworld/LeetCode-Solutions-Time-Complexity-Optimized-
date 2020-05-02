e746e3edbf73e0ff8538468be67524480d3fd15c
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-02 07:11:13 +0530

    Create 744. Find Smallest Letter Greater Than Target.cpp

---
+++ 744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,17 @@
+char nextGreatestLetter(vector<char>& letters, char target) {
+
+	int l=0, r=letters.size()-1;
+	if(letters[l]>target || letters[r]<target) return letters[l];
+	
+	while(l<r){
+	
+		int mid =l+(r-l)/2;
+		
+		if(letters[mid]<=target && letters[mid+1]>target) return letters[mid+1];
+		else if(letters[mid]<=target) l=mid+1;
+		else r=mid;
+		
+	}
+	
+	return letters[0];
+}
