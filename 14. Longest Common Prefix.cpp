5be82c4ecf1126a44337997227f498509f5746de
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-05 03:40:53 +0530

    Create 14. Longest Common Prefix.cpp

---
+++ 14. Longest Common Prefix.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0) return "";
+        string final = "";
+        bool ans = true;
+        for(int i=0; i<strs[0].size(); i++){
+            string toCheck = strs[0].substr(0,i+1);
+            for(int j=0; j<strs.size(); j++){
+                if(strs[j].substr(0,i+1).find(toCheck)!=string::npos){
+                    continue;
+                }
+                else{
+                    ans = false;
+                    break;
+                }
+            }
+            if(!ans) break;
+            final = toCheck;
+        }
+        return final;
+    }
+};
+
+
+BETTER APPROACH
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        if (strs.size() == 0) return "";
+        
+        string s = strs[0];
+        int j = 0, i = 0;
+        string ans;
+        
+        while (j < s.length()) {
+            int flag = false;
+            for (i = 1; i < strs.size(); i++) {
+                if (j > strs[i].length() || s[j] != strs[i][j]) {
+                    flag = true;
+                    break;
+                }
+            }
+            
+            if (!flag)
+                ans += s[j];
+            else
+                break;
+            j++;
+        }
+        
+        return ans;
+    }
+};
