a6d3df3f04262509ef81214b1a61545b1dd9b1ce
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-05 00:36:12 +0530

    Update 58. Length of Last Word.cpp

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -9,3 +9,23 @@ public:
         return word.size();
     }
 };
+
+
+BETTER APPROACH:
+class Solution {
+public:
+    #include <bits/stdc++.h>
+    int lengthOfLastWord(string s) {
+        if(s.size()==0) return 0;
+        int e = s.size()-1;
+        int count = 0;
+        
+        while(e>=0 && s[e]==' ') e--;
+        
+        for(int i=e; i>=0; i--){
+            if(s[i]!=' ') count++;
+            else break;
+        }
+        return count;
+    }
+};
