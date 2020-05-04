e33568bcd582741755264962e49e6d3b3c44a251
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-04 18:06:40 +0530

    Update 345. Reverse Vowels of a String.cpp

--- 345. Reverse Vowels of a String.cpp
+++ 345. Reverse Vowels of a String.cpp
@@ -18,3 +18,21 @@ public:
         return s;
     }
 };
+
+
+BETTER APPROACH:
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int n=s.size();
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            i=s.find_first_of("aeiouAEIOU",i);
+            j=s.find_last_of("aeiouAEIOU",j);
+            if(i<j)
+                swap(s[i++],s[j--]);
+        }
+        return s;
+    }
+};
