3361c85d67e854d56d0e5f0227a83dcc7f6d3d44
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-09 01:44:13 +0530

    Create 371. Sum of Two Integers.cpp

---
+++ 371. Sum of Two Integers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        string s = "", t = "", ans = "";
+        for(int i=31;i>=0;i--){
+            int k1 = a>>i;
+            if(k1&1) s+='1';
+            else s+='0';
+            int k2 = b>>i;
+            if(k2&1) t+='1';
+            else t+='0';
+            
+        }
+        int c = 0;
+        int i = s.size()-1, j = t.size()-1;
+        while(i>=0 || j>=0 || c==1){
+            c += ((i>=0) ? s[i]-'0' : 0);
+            c += ((j>=0) ? t[j]-'0' : 0);
+            ans = char(c%2 + '0')+ans;
+            c/=2;
+            i--, j--;
+        }cout<<ans;
+        
+        string num = ans;
+        long dec = 0, base = 1, len = ans.size()-1;
+        for(int i=len; i>=0; i--){
+            if(num[i]=='1') dec+=base;
+            base*=2;
+        }
+        return dec;                
+    }
+};
