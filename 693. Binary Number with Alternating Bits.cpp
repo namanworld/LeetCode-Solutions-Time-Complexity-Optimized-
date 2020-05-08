22958b8c6665526958a9100ce7311cf692f83ba9
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-09 02:16:15 +0530

    Create 693. Binary Number with Alternating Bits.cpp

---
+++ 693. Binary Number with Alternating Bits.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    #include <bits/stdc++.h>
+    bool hasAlternatingBits(int n) {
+        string s = "";
+        int dig = floor(log2(n))+1;
+        for(int i=dig-1;i>=0; i--){
+            int k = n>>i;
+            if(k & 1) s += '1';
+            else s += '0';
+        }
+        cout<<s;
+        for(int i=1; i<dig; i++){
+            if(s[i]==s[i-1]) return false;
+        }
+        return true;
+    }
+};
+
+
+#include <bits/stdc++.h>
+    bool hasAlternatingBits(int n) {
+        string s = "";
+        int lastBit=2;
+        int dig = floor(log2(n))+1;
+        for(int i=dig-1;i>=0; i--){
+            int k = n>>i;
+            if(k & 1){
+                if(lastBit == 2) lastBit = 1;
+                else if(lastBit == 1) return false;
+                lastBit = 1;
+            }
+            else{
+                if(lastBit == 2) lastBit = 0;
+                else if(lastBit == 0) return false;
+                lastBit = 0;
+            }
+        }
+        return true;
+   }
