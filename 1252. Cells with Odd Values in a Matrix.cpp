de1b607d680519c377aef2444d79b8681aefc8d4
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-13 06:23:07 +0530

    Update 1252. Cells with Odd Values in a Matrix.cpp

--- 1252. Cells with Odd Values in a Matrix.cpp
+++ 1252. Cells with Odd Values in a Matrix.cpp
@@ -2,32 +2,26 @@ class Solution {
 public:
     #include <bits/stdc++.h>
     int oddCells(int n, int m, vector<vector<int>>& indices) {
-        int A[n][m],count=0;
-        
+        int A[n][m];
         memset(A, 0, sizeof(A));
-        
-        for(int i=0;i<indices.size();i++)
-        {
-            int row=indices[i][0];
-            int col=indices[i][1];
+        for(int i=0; i<indices.size(); i++){
+            int row = indices[i][0];
+            int col = indices[i][1];
             
-            for(int j=0;j<m;j++)
+            for(int j=0; j<m; j++){
                 A[row][j]++;
-            
-            for(int j=0;j<n;j++)
+            }
+            for(int j=0; j<n; j++){
                 A[j][col]++;
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(A[i][j]%2!=0)
-                    count++;
             }
         }
         
+        int count = 0;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                if(A[i][j]%2) count++;
         
         return count;
+        
     }
 };
