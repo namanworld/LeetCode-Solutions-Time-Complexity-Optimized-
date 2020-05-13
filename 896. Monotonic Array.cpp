82ea2cf8e096793c7f1531b179dc4d43afb6c6fa
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-13 12:34:27 +0530

    Update 896. Monotonic Array.cpp

--- 896. Monotonic Array.cpp
+++ 896. Monotonic Array.cpp
@@ -12,3 +12,17 @@ public:
         return false;
     }
 };
+
+
+WITHOUT SORTING :
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        bool increasing = true, decreasing = true;
+        for(int i=0;i<A.size()-1; i++){
+            if(A[i]>A[i+1]) increasing = false;
+            if(A[i]<A[i+1]) decreasing = false;
+        }
+        return increasing || decreasing;
+    }
+};
