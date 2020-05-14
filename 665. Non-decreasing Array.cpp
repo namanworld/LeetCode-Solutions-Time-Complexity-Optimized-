2d290313373780ee349f70008c7f764d9635a53d
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-14 20:42:23 +0530

    Update 665. Non-decreasing Array.cpp

--- 665. Non-decreasing Array.cpp
+++ 665. Non-decreasing Array.cpp
@@ -14,3 +14,39 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        bool check = false;
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i]>nums[i+1]) {
+                if(check == true) return false;
+                if(i>0 && nums[i+1]<nums[i-1]) nums[i+1] = nums[i];
+                else nums[i] = nums[i+1];
+                check = true;
+            }
+        }
+        return true;
+    }
+};
+
+BETTER APPROACH:
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int n=nums.size();
+        int c=0,cmax=-1;
+        if(n==1)return true;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1]){
+                if(cmax!=-1)return false;
+                cmax=i;
+            }
+        }
+        if(cmax==0 || cmax==n-2 || cmax==-1)return true;
+        if(nums[cmax-1]<=nums[cmax+1] || nums[cmax]<=nums[cmax+2])return true;
+        return false;
+    }
+};
