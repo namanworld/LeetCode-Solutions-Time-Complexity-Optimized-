4222a217561d2da62cbb4a24f25d9ca338c935dc
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-14 09:56:38 +0530

    Update 849. Maximize Distance to Closest Person.cpp

--- 849. Maximize Distance to Closest Person.cpp
+++ 849. Maximize Distance to Closest Person.cpp
@@ -45,3 +45,37 @@ public:
         return ans;
     }
 };
+
+BETTER :
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int n = seats.size();
+        int ans = 0;
+        int k = 0;
+        
+        for(int i=0; i<n; i++){
+            if(seats[i]==1) k=0;
+            else{
+                k++;
+                ans = max(ans, (k+1)/2);
+            }
+        }
+        cout<<ans;
+        for(int i=0; i<n; i++){
+            if(seats[i]==1){
+                ans = max(ans, i);
+                break;
+            }
+        }
+        
+        for(int i=n-1; i>=0; i--){
+            if(seats[i]==1){
+                ans = max(ans, n-i-1);
+                break;
+            }
+        }
+        
+        return ans;
+    }
+};
