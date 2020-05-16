c8b0ef486e46b0eb71daf07abf47ece569d993ba
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-16 18:18:08 +0530

    Update 565. Array Nesting.cpp

--- 565. Array Nesting.cpp
+++ 565. Array Nesting.cpp
@@ -20,3 +20,26 @@ public:
         
     }
 };
+
+BETTER:
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=INT_MAX){
+                int curr = nums[i], count = 0;
+                while(nums[curr]!=INT_MAX){
+                    int temp = curr;
+                    curr = nums[curr];
+                    count++;
+                    nums[temp] = INT_MAX;
+                }
+                ans = max(ans, count);
+            }
+        }
+        return ans;
+        
+    }
+};
