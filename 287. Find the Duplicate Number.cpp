6b3fe17500f8a03872da1f5947a251236fbaf7ad
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-16 11:31:54 +0530

    Update 287. Find the Duplicate Number.cpp

--- 287. Find the Duplicate Number.cpp
+++ 287. Find the Duplicate Number.cpp
@@ -9,3 +9,42 @@ public:
         return 0;
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[abs(nums[i])]<0) return abs(nums[i]);
+            else nums[abs(nums[i])] = -nums[abs(nums[i])];
+        }
+        return 0;
+    }
+};
+
+FLOYD'S TORTOISE AND HARE:
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        
+        int tortoise = nums[0], hare = nums[0];
+        
+        do{
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while(tortoise!=hare);
+        
+        tortoise = nums[0];
+        while(tortoise!=hare){
+            tortoise = nums[tortoise];
+            hare = nums[hare];
+        }
+        return hare;
+    }
+};
