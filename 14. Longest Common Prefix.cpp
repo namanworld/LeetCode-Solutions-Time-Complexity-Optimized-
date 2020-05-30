fb127e9a3fc45bdbaa22d3f69ba58857694e98ac
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-05-30 18:02:01 +0530

    Update 14. Longest Common Prefix.cpp

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -53,3 +53,19 @@ public:
         return ans;
     }
 };
+
+BETTER:
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==0) return "";
+        
+        string res = strs[0];
+        for(int i=1; i<strs.size(); ++i){
+            int j;
+            for(j=0; j<strs[i].size() && j<res.size(); ++j) if(strs[i][j]!=res[j]) break;
+            res = res.substr(0, j);
+        }
+        return res;
+    }
+};
