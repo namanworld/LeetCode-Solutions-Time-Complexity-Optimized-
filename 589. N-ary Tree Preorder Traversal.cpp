3e89e8fcea6c36975290db533585a8041ada6368
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-06-02 16:51:54 +0530

    Update 589. N-ary Tree Preorder Traversal.cpp

--- 589. N-ary Tree Preorder Traversal.cpp
+++ 589. N-ary Tree Preorder Traversal.cpp
@@ -20,3 +20,43 @@ public:
         return v;
     }
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        if(!root) return {};
+        vector<int> res;
+        stack<Node*> s;
+        s.push(root);
+        while(!s.empty()){
+            Node* curr = s.top();
+            s.pop();
+            if(curr){
+                res.push_back(curr->val);
+                for(int i=curr->children.size()-1; i>=0; i--)
+                    s.push(curr->children[i]);
+            }
+        }
+        return res;
+    }
+};
