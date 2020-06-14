46155b54e72bb200a418526f078e53d97d4a5230
Naman Jain <nmnjn2345@gmail.com>
2020-06-14 06:47:42 +0530

    Update 1471. The k Strongest Values in an Array.cpp

--- 1471. The k Strongest Values in an Array.cpp
+++ 1471. The k Strongest Values in an Array.cpp
@@ -11,8 +11,22 @@ public:
         }
         arr.erase(arr.begin()+i, arr.begin()+j+1);
         return arr;
+    }  
+};
+
+class Solution {
+public:
+    int median;
+    bool compare(int a, int b){
+        return abs(a-median)>abs(median-b) || ((abs(a-median)==abs(median-b)) && a>b);
     }
     
-    
-    
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+        median = arr[(arr.size()-1)/2];
+        sort(arr.begin(), arr.end(), [this](int a, int b){
+           return compare(a,b); 
+        });
+        return vector<int>(arr.begin(), arr.begin()+k);
+    }  
 };
