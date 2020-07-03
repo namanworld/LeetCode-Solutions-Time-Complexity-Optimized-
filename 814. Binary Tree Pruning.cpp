ed50b077dc48bc7fb957ccb7f610aac260cab665
Naman Jain <nmnjn2345@gmail.com>
2020-07-04 00:37:39 +0530

    Create 814. Binary Tree Pruning.cpp

---
+++ 814. Binary Tree Pruning.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root) return NULL;
+        else if(!root->left && !root->right && root->val==0) return NULL;
+        
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        
+        if(!root->left && !root->right && root->val==0) return NULL;
+        
+        return root;
+    }
+};
