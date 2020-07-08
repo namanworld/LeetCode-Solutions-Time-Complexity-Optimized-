7bfbd17a4ef96df9d22e0b587ceb5658843cc5e5
Naman Jain <nmnjn2345@gmail.com>
2020-07-09 01:28:18 +0530

    Update 295. Find Median from Data Stream.cpp

--- 295. Find Median from Data Stream.cpp
+++ 295. Find Median from Data Stream.cpp
@@ -22,3 +22,35 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
+USING PRIORITY QUEUE:
+class MedianFinder {
+    priority_queue<int> lo;
+    priority_queue<int, vector<int>, greater<int> >  hi;
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {
+    }
+    
+    void addNum(int num) {
+        lo.push(num);
+        hi.push(lo.top());
+        lo.pop();
+        
+        if(lo.size() < hi.size()){
+            lo.push(hi.top());
+            hi.pop();
+        }
+    }
+    
+    double findMedian() {
+        return lo.size() > hi.size() ? lo.top() : (lo.top() + hi.top())*0.5;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
