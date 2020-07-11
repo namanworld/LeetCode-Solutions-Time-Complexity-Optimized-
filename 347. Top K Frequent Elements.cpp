6c4a420d39869caf51f3369af4b04f333863bf7e
Naman Jain <nmnjn2345@gmail.com>
2020-07-11 16:28:42 +0530

    Update 347. Top K Frequent Elements.cpp

--- 347. Top K Frequent Elements.cpp
+++ 347. Top K Frequent Elements.cpp
@@ -1,3 +1,52 @@
+O(NlogK) method by altering the priority queue.
+
+struct compare{
+    bool operator()(const pair<int,int> &x, const pair<int, int> &y){
+        return x.second > y.second;
+    }
+};
+
+class Solution {
+
+public:
+    map<int, int> mp;
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        for(int c: nums){
+            mp[c]++;
+        }
+        
+        vector<pair<int, int>> vp;
+        
+        for(auto it: mp){
+            vp.push_back({it.first, it.second});
+        }
+        
+        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+        
+        //cout<<vp.size()<<endl;
+        
+        for(auto p: vp){
+            pq.push(p);
+            //cout<<pq.size()<<endl;
+            if(pq.size() > k) pq.pop();
+        }
+        
+        vector<int> res;
+        
+        //cout<<pq.size()<<endl;
+        
+        while(pq.size()){
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        
+        return res;
+    }
+};
+
+//O(NlogN) Method:
+
 class Solution {
 public:
     
