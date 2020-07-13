833635c926aa90aaf3a38033e74e19ccb2074369
Naman Jain <nmnjn2345@gmail.com>
2020-07-14 01:53:31 +0530

    Update 819. Most Common Word.cpp

--- 819. Most Common Word.cpp
+++ 819. Most Common Word.cpp
@@ -26,3 +26,48 @@ public:
         
     }
 };
+
+#include <bits/stdc++.h>
+class Solution {
+public:
+    bool compare(pair<string, int> p, pair<string, int> q){
+        return p.second > q.second;
+    }
+    
+    string mostCommonWord(string s, vector<string>& banned) {
+        
+        set<string> st;
+        
+        for(auto str: banned){
+            st.insert(str);
+        }
+        
+        map<string, int> mp;
+        
+        string word = "";
+        for(int i=0; i<s.length(); i++){
+            if(isalpha(s[i])) word+=s[i];
+            
+            if(!isalpha(s[i]) || i==s.length()-1){
+                transform(word.begin(), word.end(), word.begin(), ::tolower);
+                if(word.length() ==0) continue;
+                mp[word]++;
+                word="";
+            }
+        }
+        
+        vector<pair<string, int>> vp;
+        for(auto m: mp){
+            vp.push_back({m.first, m.second});
+        }
+        
+        sort(vp.begin(), vp.end(), [this](pair<string, int> p, pair<string, int> q){
+           return compare(p, q) ;
+        });
+        
+        for(pair<string, int> p : vp){
+            if(st.find(p.first) == st.end()) return p.first;
+        }
+        return "";
+    }
+};
